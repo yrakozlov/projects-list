2a6ee1c9360759b03c0e861906b550ace0092fd9
yrakozlov <yurakozlov0995508414@gmail.com>
2023-05-14 15:32:22 +0300

    fix-5

--- next.config.js
+++ next.config.js
@@ -1,25 +1,4 @@
 /** @type {import('next').NextConfig} */
-const {
-  generateStaticParams,
-} = require("next/dist/next-server/lib/router/utils/generate-static-routes");
-
-const nextConfig = {
-  async rewrites() {
-    const routes = ["/index", "/about", "/contact"];
-
-    const staticParams = await Promise.all(
-      routes.map(async (route) => {
-        const { page, query } = await generateStaticParams(`/${route}`);
-        return { route, page, query };
-      })
-    );
-
-    return staticParams.map(({ route, page, query }) => ({
-      source: route,
-      destination: page,
-      query,
-    }));
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
